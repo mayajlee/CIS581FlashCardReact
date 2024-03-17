b15520ccf429a4a6ee999a3bc359ec85cad18e0f
mayajlee <mayajnailee@outlook.com>
2024-03-17 11:45:11 -0400

    flashcard pt2

--- flashcard-app/src/components/Flashcards.jsx
+++ flashcard-app/src/components/Flashcards.jsx
@@ -1,11 +1,42 @@
 import { useState } from "react"
 import "./Flashcards.css"
 function Flashcards({cards}){
-    const [curIndex, setIndex] = useState(0)
+    const [curIndex, setIndex] = useState(0);
+    const [guess, setGuess] = useState('');
+
     const bttnStyle = {backgroundColor:  "#1d4feb", margin: "10px", color: "white"}
 
+    const handleGuess = (event) => {
+        setGuess(event.target.value);
+    }
+    function resetBox(){
+        setGuess('');
+        document.getElementById("userBox").style.backgroundColor = "#ffffff";
+    }
+
+    function checkAnswer(){
+        if(guess == cards[curIndex][1]){
+            document.getElementById("userBox").style.backgroundColor = "#85e396";
+            input.style.backgroundColor = "#85e396";
+        }
+        else{
+            document.getElementById("userBox").style.backgroundColor = "#f55e5e";
+            input.style.backgroundColor = "#f55e5e";
+        }
+    }
+    
     function nextCard(){
-        setIndex(Math.floor(Math.random() * cards.length));
+        if(curIndex < cards.length -1){
+            setIndex(curIndex + 1);
+        }
+        resetBox();
+    }
+    function prevCard(){
+        if(curIndex > 0){
+            setIndex(curIndex - 1);
+        }
+        resetBox();
+        
     }
 
     return(
@@ -27,7 +58,17 @@ function Flashcards({cards}){
                     </div>
                 </div>
 
+                {/*Guess Area*/}
+                <div style={{border: "2px solid #1f52f3", padding: "20px", borderRadius: "20px"}}>
+                    <h3>Answer:</h3>
+                    <input type="text" name="" id="userBox" value = {guess} style={{padding: "5px", borderRadius: "5px"}} onChange={handleGuess}/>
+                    <button type="" style={bttnStyle} onClick={checkAnswer}>Check!</button>
+                </div>
+
+                <button style={bttnStyle} onClick={prevCard}>prev</button>
                 <button style={bttnStyle} onClick={nextCard}>next</button>
+
+                
             </div>
         </>
     )
